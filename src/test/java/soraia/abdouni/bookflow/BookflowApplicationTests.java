4eefac9ac8de16f8892a2fc621a81b4ac53f1539
Soraia Abdouni <soraiaabdounima1@gmail.com>
2026-07-01 20:56:49 -0300

    feat: adding project structure

---
+++ src/test/java/soraia/abdouni/bookflow/BookflowApplicationTests.java
@@ -0,0 +1,13 @@
+package soraia.abdouni.bookflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
